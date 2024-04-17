39e6dc33062903049222adbf23d1b511a9e521b7
chiheb <chihababiza9@gmail.com>
2024-04-17 23:41:55 +0100

    version 1 of index

--- client/index.php
+++ client/index.php
@@ -58,16 +58,23 @@ if (isset($_GET['category'])) {
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 	<meta name="author" content="Untree.co">
+
 	<meta name="description" content="" />
 	<meta name="keywords" content="bootstrap, bootstrap4" />
 
 	<!-- Bootstrap CSS -->
-	<link href="../css/bootstrap.min.css" rel="stylesheet">
 	<link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css" rel="stylesheet">
+	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+	<!-- <link href="../css/bootstrap.min.css" rel="stylesheet"> -->
 	<link href="../css/tiny-slider.css" rel="stylesheet">
 	<link href="../css/style.css" rel="stylesheet">
 	<title>Furni Free Bootstrap 5 Template for Furniture and Interior Design Websites by Untree.co </title>
 	<style>
+		.image {
+			height: 300px;
+			object-fit: contain;
+		}
+
 		.product-home {
 			display: none;
 		}
@@ -98,6 +105,14 @@ if (isset($_GET['category'])) {
 	<?php include("../inc/header.php") ?>
 	<!-- End Header/Navigation -->
 
+	<?php
+	if (isset($_SESSION['vendorName'])) {
+		echo '<h2 class="text-center my-3 fw-bolder display-6">Welcome, ';
+		echo $_SESSION['vendorName'];
+		echo '</h2>';
+	}
+	?>
+
 	<!-- Start Hero Section -->
 	<div class="hero">
 		<div class="container">
@@ -105,13 +120,9 @@ if (isset($_GET['category'])) {
 				<div class="col-lg-5">
 					<div class="intro-excerpt">
 						<h1>Modern Interior <span clsas="d-block">Design Studio</span></h1>
-						<p class="mb-4">Welcome to our vibrant multivendor marketplace, where creativity knows no
-							bounds. Uncover a world of unique products and services crafted with passion by talented
-							artisans and sellers from around the globe. Whether you're seeking handcrafted goods,
-							vintage finds, or bespoke creations, your journey starts here. Embrace the diversity,
-							support independent creators, and find something truly special that speaks to your soul.
-							Start exploring today!"</p>
-						<p><a href="" class="btn btn-secondary me-2">Become a vendor</a><a href="#" class="btn btn-white-outline">Explore</a></p>
+						<p class="mb-4">Donec vitae odio quis nisl dapibus malesuada. Nullam ac aliquet velit. Aliquam
+							vulputate velit imperdiet dolor tempor tristique.</p>
+						<p><a href="" class="btn btn-secondary me-2">Shop Now</a><a href="#" class="btn btn-white-outline">Explore</a></p>
 					</div>
 				</div>
 				<div class="col-lg-7">
@@ -143,224 +154,21 @@ if (isset($_GET['category'])) {
 	<!-- End Product Section -->
 
 	<!-- Start Why Choose Us Section -->
-	<div class="why-choose-section">
-		<div class="container">
-			<div class="row justify-content-between">
-				<div class="col-lg-6">
-					<h2 class="section-title">Why Choose Us</h2>
-					<p>Donec vitae odio quis nisl dapibus malesuada. Nullam ac aliquet velit. Aliquam vulputate velit
-						imperdiet dolor tempor tristique.</p>
-
-					<div class="row my-5">
-						<div class="col-6 col-md-6">
-							<div class="feature">
-								<div class="icon">
-									<img src="../images/truck.svg" alt="Image" class="imf-fluid">
-								</div>
-								<h3>Fast Shipping</h3>
-								<p>Donec vitae odio quis nisl dapibus malesuada. Nullam ac aliquet velit. Aliquam
-									vulputate.</p>
-							</div>
-						</div>
-
-						<div class="col-6 col-md-6">
-							<div class="feature">
-								<div class="icon">
-									<img src="../images/bag.svg" alt="Image" class="imf-fluid">
-								</div>
-								<h3>Easy to Shop</h3>
-								<p>Donec vitae odio quis nisl dapibus malesuada. Nullam ac aliquet velit. Aliquam
-									vulputate.</p>
-							</div>
-						</div>
-
-						<div class="col-6 col-md-6">
-							<div class="feature">
-								<div class="icon">
-									<img src="../images/support.svg" alt="Image" class="imf-fluid">
-								</div>
-								<h3>24/7 Support</h3>
-								<p>Donec vitae odio quis nisl dapibus malesuada. Nullam ac aliquet velit. Aliquam
-									vulputate.</p>
-							</div>
-						</div>
-
-						<div class="col-6 col-md-6">
-							<div class="feature">
-								<div class="icon">
-									<img src="../images/return.svg" alt="Image" class="imf-fluid">
-								</div>
-								<h3>Hassle Free Returns</h3>
-								<p>Donec vitae odio quis nisl dapibus malesuada. Nullam ac aliquet velit. Aliquam
-									vulputate.</p>
-							</div>
-						</div>
-
-					</div>
-				</div>
-
-				<div class="col-lg-5">
-					<div class="img-wrap">
-						<img src="../images/why-choose-us-img.jpg" alt="Image" class="img-fluid">
-					</div>
-				</div>
-
-			</div>
-		</div>
-	</div>
+	<?php include("../inc/why.php") ?>
 	<!-- End Why Choose Us Section -->
 
 	<!-- Start We Help Section -->
-	<div class="we-help-section">
-		<div class="container">
-			<div class="row justify-content-between">
-				<div class="col-lg-7 mb-5 mb-lg-0">
-					<div class="imgs-grid">
-						<div class="grid grid-1"><img src="../images/img-grid-1.jpg" alt="Untree.co"></div>
-						<div class="grid grid-2"><img src="../images/img-grid-2.jpg" alt="Untree.co"></div>
-						<div class="grid grid-3"><img src="../images/img-grid-3.jpg" alt="Untree.co"></div>
-					</div>
-				</div>
-				<div class="col-lg-5 ps-lg-5">
-					<h2 class="section-title mb-4">We Help You Make Modern Interior Design</h2>
-					<p>Donec facilisis quam ut purus rutrum lobortis. Donec vitae odio quis nisl dapibus malesuada.
-						Nullam ac aliquet velit. Aliquam vulputate velit imperdiet dolor tempor tristique. Pellentesque
-						habitant morbi tristique senectus et netus et malesuada</p>
-
-					<ul class="list-unstyled custom-list my-4">
-						<li>Donec vitae odio quis nisl dapibus malesuada</li>
-						<li>Donec vitae odio quis nisl dapibus malesuada</li>
-						<li>Donec vitae odio quis nisl dapibus malesuada</li>
-						<li>Donec vitae odio quis nisl dapibus malesuada</li>
-					</ul>
-					<p><a herf="#" class="btn">Explore</a></p>
-				</div>
-			</div>
-		</div>
-	</div>
+	<?php include("../inc/help.php") ?>
 	<!-- End We Help Section -->
 
 	<!-- Start Testimonial Slider -->
-	<div class="testimonial-section">
-		<div class="container">
-			<div class="row">
-				<div class="col-lg-7 mx-auto text-center">
-					<h2 class="section-title">Testimonials</h2>
-				</div>
-			</div>
-
-			<div class="row justify-content-center">
-				<div class="col-lg-12">
-					<div class="testimonial-slider-wrap text-center">
-
-						<div id="testimonial-nav">
-							<span class="prev" data-controls="prev"><span class="fa fa-chevron-left"></span></span>
-							<span class="next" data-controls="next"><span class="fa fa-chevron-right"></span></span>
-						</div>
-
-						<div class="testimonial-slider">
-
-							<div class="item">
-								<div class="row justify-content-center">
-									<div class="col-lg-8 mx-auto">
-
-										<div class="testimonial-block text-center">
-											<blockquote class="mb-5">
-												<p>&ldquo;Donec facilisis quam ut purus rutrum lobortis. Donec vitae
-													odio quis nisl dapibus malesuada. Nullam ac aliquet velit. Aliquam
-													vulputate velit imperdiet dolor tempor tristique. Pellentesque
-													habitant morbi tristique senectus et netus et malesuada fames ac
-													turpis egestas. Integer convallis volutpat dui quis
-													scelerisque.&rdquo;</p>
-											</blockquote>
-
-											<div class="author-info">
-												<div class="author-pic">
-													<img src="../images/person-1.png" alt="Maria Jones" class="img-fluid">
-												</div>
-												<h3 class="font-weight-bold">Maria Jones</h3>
-												<span class="position d-block mb-3">CEO, Co-Founder, XYZ Inc.</span>
-											</div>
-										</div>
-
-									</div>
-								</div>
-							</div>
-							<!-- END item -->
-
-							<div class="item">
-								<div class="row justify-content-center">
-									<div class="col-lg-8 mx-auto">
-
-										<div class="testimonial-block text-center">
-											<blockquote class="mb-5">
-												<p>&ldquo;Donec facilisis quam ut purus rutrum lobortis. Donec vitae
-													odio quis nisl dapibus malesuada. Nullam ac aliquet velit. Aliquam
-													vulputate velit imperdiet dolor tempor tristique. Pellentesque
-													habitant morbi tristique senectus et netus et malesuada fames ac
-													turpis egestas. Integer convallis volutpat dui quis
-													scelerisque.&rdquo;</p>
-											</blockquote>
-
-											<div class="author-info">
-												<div class="author-pic">
-													<img src="../images/person-1.png" alt="Maria Jones" class="img-fluid">
-												</div>
-												<h3 class="font-weight-bold">Maria Jones</h3>
-												<span class="position d-block mb-3">CEO, Co-Founder, XYZ Inc.</span>
-											</div>
-										</div>
-
-									</div>
-								</div>
-							</div>
-							<!-- END item -->
-
-							<div class="item">
-								<div class="row justify-content-center">
-									<div class="col-lg-8 mx-auto">
-
-										<div class="testimonial-block text-center">
-											<blockquote class="mb-5">
-												<p>&ldquo;Donec facilisis quam ut purus rutrum lobortis. Donec vitae
-													odio quis nisl dapibus malesuada. Nullam ac aliquet velit. Aliquam
-													vulputate velit imperdiet dolor tempor tristique. Pellentesque
-													habitant morbi tristique senectus et netus et malesuada fames ac
-													turpis egestas. Integer convallis volutpat dui quis
-													scelerisque.&rdquo;</p>
-											</blockquote>
-
-											<div class="author-info">
-												<div class="author-pic">
-													<img src="../images/person-1.png" alt="Maria Jones" class="img-fluid">
-												</div>
-												<h3 class="font-weight-bold">Maria Jones</h3>
-												<span class="position d-block mb-3">CEO, Co-Founder, XYZ Inc.</span>
-											</div>
-										</div>
-
-									</div>
-								</div>
-							</div>
-							<!-- END item -->
-
-						</div>
-
-					</div>
-				</div>
-			</div>
-		</div>
-	</div>
-	<br>
+	<?php include("../inc/comments.php") ?>
 	<!-- End Testimonial Slider -->
 
 	<!-- Start Footer Section -->
 	<?php include("../inc/footer.php") ?>
 	<!-- End Footer Section -->
 
-	<script src="../js/bootstrap.bundle.min.js"></script>
-	<script src="../js/tiny-slider.js"></script>
-	<script src="../js/custom.js"></script>
 	<script>
 		// Get the <li> element with the ID "contact"
 		var contactLi = document.getElementById('home');
@@ -368,6 +176,3 @@ if (isset($_GET['category'])) {
 		// Add classes "nav-item" and "active" to the <li> element
 		contactLi.classList.add('nav-item', 'active');
 	</script>
-</body>
-
-</html>
