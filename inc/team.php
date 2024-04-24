4ba55824206137827fb55ba815a96d45f579c3d4
chiheb <chihababiza9@gmail.com>
2024-04-24 06:41:58 +0100

    update the team

--- inc/team.php
+++ inc/team.php
@@ -1,63 +1,92 @@
-    <div class="container">
+<div class="container">
 
-        <div class="row mb-5">
-            <div class="col-lg-5 mx-auto text-center">
-                <h2 class="section-title">Our Team</h2>
-            </div>
-        </div>
-
-        <div class="row">
-
-            <!-- Start Column 1 -->
-            <div class="col-12 col-md-6 col-lg-3 mb-5 mb-md-0">
-                <img src="../images/person_1.jpg" class="img-fluid mb-5">
-                <h3 clas><a href="#"><span class="">Chiheb</span> Abiza</a></h3>
-                <span class="d-block position mb-4">CEO, Founder, Atty.</span>
-                <p>Separated they live in.
-                    Separated they live in Bookmarksgrove right at the coast of the Semantics, a large language
-                    ocean.</p>
-            </div>
-            <!-- End Column 1 -->
-
-            <!-- Start Column 2 -->
-            <div class="col-12 col-md-6 col-lg-3 mb-5 mb-md-0">
-                <img src="../images/person_2.jpg" class="img-fluid mb-5">
-
-                <h3 clas><a href="#"><span class="">Kerour</span> Amine</a></h3>
-                <span class="d-block position mb-4">CEO, Founder, Atty.</span>
-                <p>Separated they live in.
-                    Separated they live in Bookmarksgrove right at the coast of the Semantics, a large language
-                    ocean.</p>
-
-            </div>
-            <!-- End Column 2 -->
-
-            <!-- Start Column 3 -->
-            <div class="col-12 col-md-6 col-lg-3 mb-5 mb-md-0">
-                <img src="../images/person_3.jpg" class="img-fluid mb-5">
-                <h3 clas><a href="#"><span class="">Patrik</span> White</a></h3>
-                <span class="d-block position mb-4">CEO, Founder, Atty.</span>
-                <p>Separated they live in.
-                    Separated they live in Bookmarksgrove right at the coast of the Semantics, a large language
-                    ocean.</p>
-            </div>
-            <!-- End Column 3 -->
-
-            <!-- Start Column 4 -->
-            <div class="col-12 col-md-6 col-lg-3 mb-5 mb-md-0">
-                <img src="../images/person_4.jpg" class="img-fluid mb-5">
-
-                <h3 clas><a href="#"><span class="">Kathryn</span> Ryan</a></h3>
-                <span class="d-block position mb-4">CEO, Founder, Atty.</span>
-                <p>Separated they live in.
-                    Separated they live in Bookmarksgrove right at the coast of the Semantics, a large language
-                    ocean.</p>
-
-
-            </div>
-            <!-- End Column 4 -->
-
-
-
+    <div class="row mb-5">
+        <div class="col-lg-12 mx-auto text-center">
+            <h2 class="section-title">Our Team</h2>
         </div>
     </div>
+
+    <div class="row team-cards-container">
+        <!-- Start Column 1 -->
+        <div class="col-12 col-md-6 col-lg-4 mb-5 mb-md-0">
+            <div class="card">
+                <img src="../images/person_2.jpg" class="card-img-top" alt="Chiheb Abiza">
+                <div class="card-body">
+                    <h5 class="card-title">Chiheb ABIZA</h5>
+                    <p class="card-text">Project Manager</p>
+                    <p class="card-text">Chiheb leads our project development team with a vision to create innovative solutions that solve real-world problems.</p>
+                </div>
+            </div>
+            <br>
+        </div>
+        <!-- End Column 1 -->
+
+        <!-- Start Column 2 -->
+        <div class="col-12 col-md-6 col-lg-4 mb-5 mb-md-0">
+            <div class="card">
+                <img src="../images/person_4.jpg" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">Belkis KANTOUR</h5>
+                    <p class="card-text">Back-end Developer</p>
+                    <p class="card-text">Belkis is our skilled back-end developer, responsible for building and maintaining the server-side logic of our web applications.</p>
+                </div>
+            </div>
+        </div>
+        <!-- End Column 2 -->
+
+        <!-- Start Column 3 -->
+        <div class="col-12 col-md-6 col-lg-4 mb-5 mb-md-0">
+            <div class="card">
+                <img src="../images/person_3.jpg" class="card-img-top" alt="Said Houidech">
+                <div class="card-body">
+                    <h5 class="card-title">Said HAOUIDECHE</h5>
+                    <p class="card-text">Front-end Developer</p>
+                    <p class="card-text">Said is responsible for building the user-facing aspects of our web applications, focusing on layout, design, and interactivity.</p>
+                </div>
+            </div>
+        </div>
+
+        <!-- End Column 3 -->
+
+        <!-- Start Column 4 -->
+        <div class="col-12 col-md-6 col-lg-4 mb-5 mb-md-0">
+            <div class="card">
+                <img src="../images/person_4.jpg" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">Dania BOUROUIH</h5>
+                    <p class="card-text">UI/UX Designer</p>
+                    <p class="card-text">Dania is responsible for designing the overall look and feel of our web applications and creating visually appealing interfaces.</p>
+                </div>
+            </div>
+        </div>
+        <!-- End Column 4 -->
+
+        <!-- Start Column 5 -->
+        <div class="col-12 col-md-6 col-lg-4 mb-5 mb-md-0">
+            <div class="card">
+                <img src="../images/person_1.jpg" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">Amine KEROUR</h5>
+                    <p class="card-text">Back-end Developer</p>
+                    <p class="card-text">Amine is our skilled back-end developer, responsible for building and maintaining the server-side logic of our web applications.</p>
+                </div>
+            </div>
+        </div>
+        <!-- End Column 5 -->
+
+        <!-- Start Column 6 -->
+        <div class="col-12 col-md-6 col-lg-4 mb-5 mb-md-0">
+            <div class="card">
+                <img src="../images/person_4.jpg" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">Minet Errahmane MOUFOK</h5>
+                    <p class="card-text">Content Manager / Copywriter</p>
+                    <p class="card-text">Minet Errahmane is responsible for managing the content displayed on our web applications. She aligned with the project's goals.</p>
+                </div>
+            </div>
+        </div>
+        <!-- End Column 6 -->
+
+    </div>
+</div>
+<br><br>
