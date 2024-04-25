23047253afc8ce9083f37bd46b81c0da7e4a2db0
chiheb <chihababiza9@gmail.com>
2024-04-25 03:57:41 +0100

    convert poinsts version 1

--- vendor/convert.php
+++ vendor/convert.php
@@ -24,24 +24,27 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     header("Location: dashboard.php");
     exit();
 }
+
+// Include the head
+include("../inc/head.php");
+
+// Include the header
+include("../inc/dash.php");
 ?>
+<div class="container mt-5">
+    <h3 class="text-center">Convert Points to Balance</h3>
+    <br>
+    <div class="row justify-content-center">
+        <div class="col-md-8 col-lg-8 pb-4">
+            <p class="text-center display-7">Are you sure you want to convert your points to balance?</p>
+            <br>
+            <form action="" method="post">
+                <button class="btn btn-primary w-100" type="submit">Convert</button>
+            </form>
+        </div>
+    </div>
+</div>
 
-
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Convert Points to Balance</title>
-</head>
-
-<body>
-    <h2>Convert Points to Balance</h2>
-    <form action="" method="post">
-        <p>Are you sure you want to convert your points to balance?</p>
-        <button type="submit">Convert</button>
-    </form>
-</body>
-
-</html>
+<script src="../js/bootstrap.bundle.min.js"></script>
+<script src="../js/tiny-slider.js"></script>
+<script src="../js/custom.js"></script>
